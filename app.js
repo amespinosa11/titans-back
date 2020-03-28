a84b006be48f86ff85c886e603269c5d156fb1c8
jprieto_kinpos <jprieto@kinpos.com>
2020-03-28 11:47:00 -0500

    Primeros servicios tipo get que se crean para el proyecto.

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 const express = require('express');
 const app = express();
 
-const bodyParser  = require("body-parser");
+const bodyParser = require("body-parser");
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Routes
-const exampleRoutes = require("./services/example/api")
+const routes = require("./services/example/api")
 
-app.use("/example", exampleRoutes);
+app.use("/testingTool", routes);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+/*app.put('/usuario', function (req, res) {
+   res.send(respuesta);
 });
-
-
+app.delete('/usuario', function (req, res) {
+    res.send(respuesta);
+});*/
 
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
--- services/example/api.js
+++ services/example/api.js
@@ -1,12 +1,60 @@
 const express = require("express");
 const router  = express.Router();
-const exampleModel = require('./model');
-const ExampleModel = new exampleModel();
+const testingModel = require('./model');
+const TestingModel = new testingModel();
 
-router.get("/", async (req, res) => {
-    let exampleData = await ExampleModel.getExample();
-    console.log(exampleData);
-    res.send(exampleData)
+router.get("/getVersions", async (req, res) => {
+
+    let versions = await TestingModel.getVersions();
+    console.log(versions);
+    res.send(versions)
+    
+});
+router.get("/getApplications", async (req, res) => {
+
+    let applications = await TestingModel.getApplications();
+    console.log(applications);
+    res.send(applications)
+    
+});
+router.get("/getToolsAvailable", async (req, res) => {
+
+    let toolsAvailable = await TestingModel.getToolsAvailable();
+    console.log(toolsAvailable);
+    res.send(toolsAvailable)
+    
+});
+router.get("/getTypeOfTests", async (req, res) => {
+
+    let typeOfTests = await TestingModel.getTypeOfTests();
+    console.log(typeOfTests);
+    res.send(typeOfTests)    
+});
+router.get("/getStrategies", async (req, res) => {
+
+    let strategies = await TestingModel.getStrategies();
+    console.log(strategies);
+    res.send(strategies)    
 });
 
-module.exports = router;
+router.get("/getTestState/:testId", async (req, res) => {
+    
+    console.log(req.params.testId);
+    let state = await TestingModel.getTestState(req.params.testId);
+    console.log(state);
+    res.send(state)   
+});
+router.get("/getTestResult/:testId", async (req, res) => {
+    
+    console.log(req.params.testId);
+    let result = await TestingModel.getTestResult(req.params.testId);
+    console.log(result);
+    res.send(result)   
+});
+
+router.post('/usuario', function (req, res) {
+    console.log(req.body);
+    res.send('POST request to homepage')
+});
+
+module.exports = router
